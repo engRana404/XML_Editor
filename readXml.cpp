3a230b7f02dcdfe42931fd51fa40abe91db5825d
NadaTarekk <105301371+NadaTarekk@users.noreply.github.com>
2022-11-15 22:56:23 +0200

    Create readXml.cpp

---
+++ readXml.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int read_words(vector<string>& words, ifstream& in,string&s)
+{
+    int i = 0;
+    while (!in.eof()) {
+        in >> words[i];
+        s += words[i]+" ";
+        i++;
+    }
+    return i - 1;
+}
+
+int main()
+{
+    ifstream ifp("C:\\Users\\Win10\\Desktop\\senoir 1\\sample.xml");
+
+    vector<string> w(500);
+    string xml;
+    int number_of_words = read_words(w, ifp,xml);
+    w.resize(number_of_words);
+    cout << xml;
+ }
