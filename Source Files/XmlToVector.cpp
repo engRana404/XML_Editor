bf658af4b857512a7c8aa765fb70f862fd9aace3
Omnia Ahmed <74054844+OmniaAhmed292@users.noreply.github.com>
2022-12-07 20:32:35 +0200

    Update XmlToVector.cpp

--- Source Files/XmlToVector.cpp
+++ Source Files/XmlToVector.cpp
@@ -9,31 +9,21 @@ using namespace std;
 We want to remove spaces from start and end of the line only but not the spaces inside the line;
 */
 string removeSpaces(string line){
-    //The line without spaces.
-    string result_line = "";
-
-    //Start of the line
     int start = 0;
-    //End of the line
+    while(line[start] == ' ' || line[start] == '\t' || line[start] == '\n'){
+        start += 1;
+    }
     int end = line.length() - 1;
-
-    while(line[start] == ' ' || line[start] == '\n' || line[start] == '\t'){
-        //Skip spaces in the start of the line
-        start++;
+    while(line[end] == ' ' || line[end] == '\t' || line[end] == '\n') {
+        end -= 1;
     }
-
-    while(line[end] == ' ' || line[end] == '\n' || line[end] == '\t') {
-       //Skip spaces in the end of the line
-        end--;
-    }
-
+    string result = "";
     for(int i = start ; i <= end; i++){
-       //Add only the part without the start and end lines
-        result_line += line[i];
+        result += line[i];
     }
-
-    return result_line;
+    return result;
 }
+
 vector<string> convert2vector(string xmlFile){
     vector <string> xmlVector;
     //Loop over the whole file
