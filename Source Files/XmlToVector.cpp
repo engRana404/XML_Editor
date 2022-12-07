b4772dc97bdc9e620163f2bcedae2871bba141a6
Omnia Ahmed <74054844+OmniaAhmed292@users.noreply.github.com>
2022-12-07 20:10:01 +0200

    Update XmlToVector.cpp

--- Source Files/XmlToVector.cpp
+++ Source Files/XmlToVector.cpp
@@ -1,44 +1,39 @@
+using namespace std;
 #include <iostream>
 #include <fstream>
 #include <string>
 #include <vector>
-using namespace std;
 
-/* 
-We want to remove spaces from start and end of the line only but not the spaces inside the line; 
+
+/*
+We want to remove spaces from start and end of the line only but not the spaces inside the line;
 */
 string removeSpaces(string line){
-    //The line without spaecs.
+    //The line without spaces.
     string result_line = "";
-  
+
     //Start of the line
     int start = 0;
-    //End of the line 
+    //End of the line
     int end = line.length() - 1;
-    
+
     while(line[start] == ' ' || line[start] == '\n' || line[start] == '\t'){
         //Skip spaces in the start of the line
         start++;
     }
-    
+
     while(line[end] == ' ' || line[end] == '\n' || line[end] == '\t') {
        //Skip spaces in the end of the line
         end--;
     }
-    
+
     for(int i = start ; i <= end; i++){
        //Add only the part without the start and end lines
         result_line += line[i];
     }
-  
+
     return result_line;
 }
-
-
-/*
-Turn string into vector
-*/
-
 vector<string> convert2vector(string xmlFile){
     vector <string> xmlVector;
     //Loop over the whole file
@@ -84,3 +79,16 @@ vector<string> convert2vector(string xmlFile){
     }//End of whole file loop
     return xmlVector;
 }
+
+int main()
+{
+
+    string xml = "<root>        <name>john</name>        <loc>brazil</loc>       <age>95 </age >        </entry>        <entry>       <foo>charles</foo>";
+    vector<string> xmlVector;
+    xmlVector = convert2vector(xml);
+    cout << xmlVector[1];
+
+
+    return 0;
+}
+
