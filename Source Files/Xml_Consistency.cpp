c21f1abcd93d7977f3a8c270f5d244415728db72
Nada Amin <104012796+Nada-Amin@users.noreply.github.com>
2022-12-14 15:43:41 +0200

    with main for testing

--- Source Files/Xml_Consistency.cpp
+++ Source Files/Xml_Consistency.cpp
@@ -1,10 +1,88 @@
 #include <iostream>
 #include <stack>
 #include <fstream>
-#include "XmlToTree.h"
+#include <string>
+#include <vector>
+/*#include "XmlToTree.h"
 #include "xml Formatting.h"
-#include "XmlToVector.h"
+#include "XmlToVector.h"*/
 using namespace std;
+
+//function that reads all text in a file and put it in a string and returns the number of words
+int read_words(vector<string>& words, ifstream& in,string&s)
+{
+    int i = 0;
+    while (!in.eof()) {
+        in >> words[i];
+        s += words[i]+" ";
+        i++;
+    }
+    return i - 1;
+}
+/*
+We want to remove spaces from start and end of the line only but not the spaces inside the line;
+*/
+string removeSpaces(string line){
+    int start = 0;
+    while(line[start] == ' ' || line[start] == '\t' || line[start] == '\n'){
+        start += 1;
+    }
+    int end = line.length() - 1;
+    while(line[end] == ' ' || line[end] == '\t' || line[end] == '\n') {
+        end -= 1;
+    }
+    string result = "";
+    for(int i = start ; i <= end; i++){
+        result += line[i];
+    }
+    return result;
+}
+
+vector<string> convert2vector(string xmlFile){
+    vector <string> xmlVector;
+    //Loop over the whole file
+    for(int i=0; i<xmlFile.length();i++){
+            string XmlTemp="";
+            if(xmlFile[i]=='<'){
+                while(xmlFile[i]!='>'){
+                    //add characters of the wanted tag
+                    XmlTemp+=xmlFile[i];
+                    //Go to next char.
+                    i++;
+                    }//End of while == We have <word inside temp
+                    //Add '>' to finish the tag
+                    XmlTemp+='>';
+                    i++;
+                    //Clean line from unwanted spaces
+                    XmlTemp= removeSpaces(XmlTemp);
+                    //Push tag into vector
+                    xmlVector.push_back(XmlTemp);
+                    //Clean temp variable
+                    XmlTemp= "";
+                    } //End of if char is <
+            //if it's not <
+            else {
+                    //continue;
+                    while(xmlFile[i]!='<'){
+                        //add characters of the wanted tag
+                       if(xmlFile[i]!='\n'&& xmlFile[i]!='\t') XmlTemp+=xmlFile[i];
+                        //Go to next char.
+                        i++;
+
+                    }
+                    XmlTemp= removeSpaces(XmlTemp);
+                    if(XmlTemp!=""){xmlVector.push_back(XmlTemp);}
+                    XmlTemp="";
+
+
+
+            }//End of else
+            i--;
+
+
+    }//End of whole file loop
+    return xmlVector;
+}
 
 //IsConsistent function returns false in case of missing closing or opening tags or mismatching tags
 bool IsConsistent(vector<string> xmlFile){
@@ -57,9 +135,14 @@ bool IsConsistent(vector<string> xmlFile){
 
 int main(){
     ifstream ifp("C:/Users/oem/Desktop/DS_Project/sample.xml");
-    vector<string> w;
+    vector<string> w(500);
     string xml;
     read_words(w, ifp, xml);
+
+    //cout<<xml;
+    /*vector<string> xmlFile=convert2vector(xml);
+    for(int i = 0; i < xmlFile.size(); i++)
+        cout << xmlFile[i] << ' ';*/
     cout<<IsConsistent(convert2vector(xml));
 
 return 0;
