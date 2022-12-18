fad990190b4e932f7968b5ad224db9f04deba591
Nada Amin <104012796+Nada-Amin@users.noreply.github.com>
2022-12-18 14:20:32 +0200

    Update and rename CorrectVector.cpp to CorrectErr.cpp

--- Source Files/CorrectErr.cpp
+++ Source Files/CorrectErr.cpp
@@ -1,10 +1,4 @@
-#include <iostream>
-#include <stack>
-#include <fstream>
-#include <string>
-#include <vector>
-#include <map>
-#include <sstream>
+#include "CorrectErr.h"
 using namespace std;
 //Function that reads all text in a file (inputFile) and put it in a vector of strings (words) and returns the number of words (i)
 bool isNumber(const string& s)
@@ -23,67 +17,6 @@ int read_words(vector<string>& words, ifstream& in,string&s)
     }
     return i - 1;
 }
-string removeSpaces(string line){
-    int start = 0;
-    while(line[start] == ' ' || line[start] == '\t' || line[start] == '\n'){
-        start += 1;
-    }
-    int end = line.length() - 1;
-    while(line[end] == ' ' || line[end] == '\t' || line[end] == '\n') {
-        end -= 1;
-    }
-    string result = "";
-    for(int i = start ; i <= end; i++){
-        result += line[i];
-    }
-    return result;
-}
-vector<string> convert2vector(string xmlFile){
-    vector <string> xmlVector;
-    //Loop over the whole file
-    for(int i=0; i<xmlFile.length();i++){
-            string XmlTemp="";
-            if(xmlFile[i]=='<'){
-                while(xmlFile[i]!='>'){
-                    //add characters of the wanted tag
-                    XmlTemp+=xmlFile[i];
-                    //Go to next char.
-                    i++;
-                    }//End of while == We have <word inside temp
-                    //Add '>' to finish the tag
-                    XmlTemp+='>';
-                    i++;
-                    //Clean line from unwanted spaces
-                    XmlTemp= removeSpaces(XmlTemp);
-                    //Push tag into vector
-                    xmlVector.push_back(XmlTemp);
-                    //Clean temp variable
-                    XmlTemp= "";
-                    } //End of if char is <
-            //if it's not <
-            else {
-                    //continue;
-                    while(xmlFile[i]!='<'){
-                        //add characters of the wanted tag
-                       if(xmlFile[i]!='\n'&& xmlFile[i]!='\t') XmlTemp+=xmlFile[i];
-                        //Go to next char.
-                        i++;
-
-                    }
-                    XmlTemp= removeSpaces(XmlTemp);
-                    if(XmlTemp!=""){xmlVector.push_back(XmlTemp);}
-                    XmlTemp="";
-
-
-
-            }//End of else
-            i--;
-
-
-    }//End of whole file loop
-    return xmlVector;
-}
-
 vector<string> CorrectErrors(vector<string> XmlVector){
     vector<string> result=XmlVector;
     stack <string> tag;
@@ -126,6 +59,7 @@ vector<string> CorrectErrors(vector<string> XmlVector){
                                //result[Lnum.top()]="<"+tag.top();
                             }
                             else{
+                                //<id>1</name>
                                 string old=result[i];
                                 result[i]="</"+tag.top();
                                 result.resize(result.size()+1);
@@ -145,26 +79,4 @@ vector<string> CorrectErrors(vector<string> XmlVector){
         }
     }
     return result;
-}
-
-int main(){
-    ifstream ifp("C:/Users/oem/Desktop/DS_Project/sample.xml");
-    vector<string> w(500);
-    string xml;
-    read_words(w, ifp, xml);
-
-    //cout<<xml;
-    /*vector<string> xmlFile=convert2vector(xml);
-    for(int i = 0; i < xmlFile.size(); i++)
-        cout << xmlFile[i] << ' ';*/
-    vector<string> XMLvector=convert2vector(xml);
-
-    for(int i = 0; i < XMLvector.size(); i++)
-        cout << XMLvector[i];
-    XMLvector=CorrectErrors(XMLvector);
-    cout<<endl;
-    for(int i = 0; i < XMLvector.size(); i++)
-        cout << XMLvector[i];
-
-return 0;
 }
