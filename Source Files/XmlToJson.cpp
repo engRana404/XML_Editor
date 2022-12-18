5db7d2b1c4defdcaef080575ab8d87349cf3a482
NadaTarekk <105301371+NadaTarekk@users.noreply.github.com>
2022-12-18 14:11:55 +0200

    Update XmlToJson.cpp

--- Source Files/XmlToJson.cpp
+++ Source Files/XmlToJson.cpp
@@ -12,96 +12,102 @@ string getString(string r)
 	r = "\"" + r + "\":";
 	return r;
 }
-
 //convert xml to json vector 
 void xmlToJson(Node* root, vector <string>& json)
 {    
 	stack <string> brackets;                    // stack to keep track of brackts
 	stack <int> num_of_children;               // stack to keep track of no. of childern
-	vector <Node*> x = Traversal(root);  // x is vector of all nodes in the tree
-
+	vector <Node*> x = depthTraversal(root);  // x is vector of all nodes in the tree
+	bool u=true;
 	for (int i = 0; i < x.size(); i++)
 	{
 		if (i == 0) // root of tree
 		{
 			json.push_back("{");
-			json.push_back("\n");
+			json.push_back("\n"); 
 		}
 		//node with more than child with diffrent tag names
-		if (x[i]->get_children().size() > 1 && diffChildern(x[i])) {
+		if (x[i]->getchildren().size() > 1 && diffChildern(x[i])) {
+			if (u==false || i==0) {
+				json.push_back(getString(x[i]->gettagname()));
+			}
 			json.push_back("{");
 			json.push_back("\n");
-			json.push_back(getString(x[i]->get_children()[0]->get_name()));
+			//json.push_back(getString(x[i]->getchildren()[0]->gettagname()));
 			brackets.push("}");
 		}
 		//node with more than child with same tag name
-		if (x[i]->get_children().size() > 1) {
+		if (x[i]->getchildren().size() > 1) {
 
 			if (!diffChildern(x[i]))
 			{
 
-				json.push_back(getString(x[i]->get_name()));
+				json.push_back(getString(x[i]->gettagname()));
 
 				json.push_back("{");
 				json.push_back("\n");
 
 				brackets.push("}");
-				json.push_back(getString(x[i]->get_children()[0]->get_name()));
+				json.push_back(getString(x[i]->getchildren()[0]->gettagname()));
 
 				json.push_back("[");
 				json.push_back("\n");
 
 				brackets.push("]");
-
+				u == false;
 
 			}
 		}
 		//node with one child
-		if (x[i]->get_children().size() == 1) {
-			if (!(x[i]->get_name() == "<follower>"))
+		if (x[i]->getchildren().size() == 1) {
+			
+			if (!(x[i]->gettagname() == "<follower>") )
 			{
-				json.push_back(getString(x[i]->get_name()));
+				json.push_back(getString(x[i]->gettagname()));
 			}
 
 			json.push_back("{");
 			json.push_back("\n");
-			json.push_back(getString(x[i]->get_children()[0]->get_name()));
+			if (!(x[i]->gettagname() == "<follower>"))
+			{
+				json.push_back(getString(x[i]->getchildren()[0]->gettagname()));
+			}
 			brackets.push("}");
 
 
 		}
 		
-		if (x[i]->get_children().size() > 0)
+		if (x[i]->getchildren().size() > 0)
 		{
 			// root of tree
 			if (i == 0) {
-				num_of_children.push(x[i]->get_children().size());
+				num_of_children.push(x[i]->getchildren().size());
 			}
 			//put current node childern number and decrease previous node childern by 1
 			if (i != 0) {
 				int z = num_of_children.top() - 1;
 				num_of_children.pop();
 				num_of_children.push(z);
-				num_of_children.push(x[i]->get_children().size());
+				num_of_children.push(x[i]->getchildren().size());
 
 			}
 		}
 
-		if (x[i]->get_children().size() == 0)
+		if (x[i]->getchildren().size() == 0)
 
 		{
-			if ((x[i]->get_name()) == "<name>") {
-				json.push_back(getString(x[i]->get_name()));
+			if ((x[i]->gettagname()) != "<topic>") {
+				json.push_back(getString(x[i]->gettagname()));
 			}
 
-			json.push_back(x[i]->get_data());
+			json.push_back(x[i]->getdata());
 
 
 			//pop top bracket when all childern are finished
 			int z = num_of_children.top() - 1;
 			if (z == 0) {
 				string j = brackets.top();
-				//json.push_back("\n");
+				json.push_back("\n");
 				json.push_back(brackets.top());
 				json.push_back("\n");
 				brackets.pop();
@@ -125,21 +131,24 @@ void xmlToJson(Node* root, vector <string>& json)
 				if (num_of_children.top() == 0) {
 
 					num_of_children.pop();
-					int x = num_of_children.top();
+					if (num_of_children.size() != 0) {
+						int x = num_of_children.top();
 
-					if (num_of_children.top() == 0) {
+						if (num_of_children.top() == 0) {
 
-						string b = brackets.top();
-						json.push_back(brackets.top());
-						brackets.pop();
-						json.push_back(",");
-						json.push_back("\n");
-
-						if (b == "]") {
-
+							string b = brackets.top();
 							json.push_back(brackets.top());
-							json.push_back("\n");
 							brackets.pop();
+							json.push_back(",");
+							json.push_back("\n");
+
+							if (b == "]") {
+
+								json.push_back(brackets.top());
+								//json.push_back(",");
+								json.push_back("\n");
+								brackets.pop();
+							}
 						}
 					}
 				}
@@ -166,7 +175,8 @@ string VectorToString(vector <string> x)
 	{
 		s = s + x[i];
 		if (x[i] == "{" || x[i] == "[") z++; 
-		if (x[i] == "}" || x[i] == "]") z--;
+		if(i<x.size()-1)
+		if (x[i+1] == "}" || x[i+1] == "]") z--;
 		if (x[i] == "\n") {
 			for (int n = 1; n<= z; n++)
 			{
