717f716b85005feeefc3e366bad3c570745bdc07
engRana404 <1901398@eng.asu.edu.eg>
2022-12-18 10:57:49 +0200

    update Formatting.h

--- Header Files/Formatting.h
+++ Header Files/Formatting.h
@@ -1,4 +1,9 @@
 #pragma once
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
 bool findChar(char x, string y);
 string formatting(vector<string> xmlFile);
 string formatting2(string xmlFile);
