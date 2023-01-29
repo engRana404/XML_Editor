085fedaf09b0c7ad5893ff59ea6c824acc2b6064
Nada Amin <104012796+Nada-Amin@users.noreply.github.com>
2023-01-29 20:50:46 +0200

    add correct errors function

---
+++ Source Files/main.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <stack>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <map>
+#include "readXml.h"
+#include "XmlToVector.h"
+using namespace std;
+map<int, string> CorrectErr(vector<string> xml){
+    map<int, string> result;
+    stack<string> tag;
+    stack<int> lineNum;
+        int i = 0;
+        for (i = 0; i < xml.size(); i++) {
+            string l = xml[i];
+            if (l[0] == '<') {
+                if (l[1] == '/') {
+                    if(!tag.empty()){
+                        if (l != ("</" + tag.top())) {
+                            // tags mismatch --> make a closing tag that fits the opening one
+                            result[i] = "</" + tag.top();
+                            tag.pop();
+                            lineNum.pop();
+                        }
+                        else {
+                            tag.pop();
+                            lineNum.pop();
+                        }
+                    }
+                    else{
+                        // closing tag without opening tag    --->   remove it
+                        result.erase(i);
+                    }
+                }
+                else if(l[1] != '!' && l[1] != '?') {
+                    if(!(l[l.length()-2] == '/' && l[l.length()-1] == '>')){
+                        string tmp = "";
+                        for(int j=1; j<l.length(); j++){
+                            if(l[j] != ' ' && l[j] != '>'){
+                             tmp += l[j];
+                            }
+                            else{
+                                tmp += '>';
+                                break;
+                            }
+                        }
+                        tag.push(tmp);
+                        lineNum.push(i);
+                    }
+                }
+            }
+            else{
+                // no closing tag -> add it
+                if(i+1 < xml.size()){
+                    string closingTag = xml[i+1]; //must be closing tag
+                    if(!(closingTag[0] == '<' && closingTag[1] == '/')){
+                        if(!tag.empty()){
+                            result[i] = "</" + tag.top();
+                            tag.pop();
+                            lineNum.pop();
+                        }
+                    }
+                }
+            }
+        }
+        return result;
+}
+int main(){
+    ifstream ifp("C:/Users/oem/Desktop/Files/DS_Project/sample.xml");
+    vector<string> w(500);
+    string xml;
+    readWords(w, ifp, xml);
+    cout<<xml<<endl;
+    vector<string> XMLvector=convert2vector(xml);
+    for(const auto& elem : CorrectErr(XMLvector))
+    {
+        cout << elem.first << " " << elem.second<< "\n";
+    }
+
+return 0;
+}
