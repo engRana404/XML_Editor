8fa9dc9f0054a7c33fa1902a95d9bbc23e1ad683
Nada Amin <104012796+Nada-Amin@users.noreply.github.com>
2023-02-03 12:10:32 +0200

    Add files via upload

---
+++ Header Files/Search_Post.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <string>
+#include <vector>
+#include <fstream>
+#include <stack>
+#include <iostream>
+#include "XmlToVector.h"
+#include "XmlToTree.h"
+int Search_String(string str,string SearchWord);
+void Search_Post(vector <string> XmlVector,string word);
