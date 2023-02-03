5729f6e82fcc135670de8bd1dde7b2c759e77d29
Nada Amin <104012796+Nada-Amin@users.noreply.github.com>
2023-02-03 17:08:19 +0200

    add CorrectErr.h

---
+++ Header Files/CorrectErr.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <iostream>
+#include <stack>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <map>
+#include "readXml.h"
+#include "XmlToVector.h"
+using namespace std;
+map<int, string> CorrectErr(vector<string> xml);
