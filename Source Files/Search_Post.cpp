245d83d1448e1ff25a5c3967c4904505fbd025ae
Nada Amin <104012796+Nada-Amin@users.noreply.github.com>
2023-02-03 12:10:16 +0200

    Add files via upload

---
+++ Source Files/Search_Post.cpp
@@ -0,0 +1,48 @@
+#include "Search_Post.h"
+
+int Search_String(string str,string SearchWord){
+        size_t found = str.find(SearchWord);
+        if (found != string::npos)
+            return 1;
+        return -1;
+}
+void Search_Post(vector <string> XmlVector,string word){
+    Node* root = new Node();
+    root->set_name(XmlVector[1]);
+    int k{ 2 };
+    xml2tree(root, XmlVector, k, root);
+    vector <Node*> tree = Traversal(root);
+    string SearchWord=word;
+    root->set_name(tree[0]->get_name());
+	vector<Node*>tags_children = root->get_children();
+	vector<Node*>posts(0);
+    Node* temp=new Node();
+    vector<Node*>p(0);
+    vector<Node*>topics(0);
+    for (int i = 0; i < tags_children.size(); i++) {
+			if (tags_children[i]->get_name() == "<user>"){
+                if((tags_children[0]->get_children())[2]->get_name()=="<posts>")
+                    posts.push_back((tags_children[0]->get_children())[2]);
+			}
+    }
+    for (int i = 0; i < posts.size(); i++) {
+        for(int j=0;j<(posts[i]->get_children()).size();j++){
+            if((posts[i]->get_children())[j]->get_name()=="<post>")
+            {
+                p.push_back((posts[i]->get_children())[j]);
+            }
+        }
+
+    }
+
+    for(int i=0;i<p.size();i++){
+        for(int j=0;j<(p[i]->get_children()).size();j++){
+            if((p[i]->get_children())[j]->get_name()=="<body>"){
+                if(Search_String((p[i]->get_children())[j]->get_data(),SearchWord)==1)
+                    cout<<"Body:"<< (p[i]->get_children())[j]->get_data()<<endl;
+            }
+        }
+
+    }
+
+}
